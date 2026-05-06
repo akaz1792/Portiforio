fced2bec9c0530f5b3a33f37284a1dd37d4b9b13
akaz2407 <akaz.guerra24@gmail.com>
2026-05-06 20:30:36 -0300

    Corrige gitignore e remove node_modules

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 document.getElementById("botao").addEventListener("click", async function () {
     const nome = document.getElementById("nome").value.trim();
     const email = document.getElementById("email").value.trim();
@@ -70,36 +70,3 @@ document.getElementById("botao").addEventListener("click", async function () {
         document.body.classList.toggle("light-mode");
     });
 
-=======
-document.addEventListener("DOMContentLoaded", function () {
-
-    document.getElementById("botao").addEventListener("click", function() {
-
-        var nome = document.getElementById("nome").value;
-        var email = document.getElementById("email").value;
-        var mensagem = document.getElementById("mensagem").value;
-        var assunto = document.getElementById("assunto").value;
-
-        const corpo = 
-            `${mensagem}`;
-
-        const gmailLink = 
-            `https://mail.google.com/mail/?view=cm&fs=1` +
-            `&to=akaz.guerra24@gmail.com` +
-            `&su=${encodeURIComponent(assunto)}` +
-            `&body=${encodeURIComponent(corpo)}`;
-
-        window.open(gmailLink, "_blank");
-    });
-
-
-    document.getElementById("light").addEventListener("click", function() {
-        document.body.classList.toggle("light-mode");
-    })
-
-    document.getElementById("dark").addEventListener("click", function() {
-        document.body.classList.toggle("light-mode");
-    });
-
-});
->>>>>>> b694a83e229b21da02d6266b911e28077864f6a0
